ad062a03016a6f861d8a1e8fb479ec0db986427d
vovachka25 <93083221+vovachka25@users.noreply.github.com>
2022-12-15 18:52:41 +0400

    update selenide homework

--- src/test/java/SelenideBooksTest.java
+++ src/test/java/SelenideBooksTest.java
@@ -1,17 +1,15 @@
-import com.codeborne.selenide.*;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.testng.ScreenShooter;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Configuration.*;
-import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideBooksTest {
@@ -40,30 +38,37 @@ public class SelenideBooksTest {
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(books.size(), 10);
 
+
         books
                 .get(0)
                 .$("[role=\"gridcell\"]",1)
-                .shouldHave(Condition.text("Learning JavaScript Design Patterns"));
+                .shouldHave(text("Learning JavaScript Design Patterns"));
 
         books
                 .stream()
-                .forEach(el -> el.$("[role=\"gridcell\"]",1).$("a").click());
+                .forEach(el -> {el.$("[role=\"gridcell\"]", 1).$("a").scrollTo().click();back();} );
 
         softAssert.assertAll();
     }
 
-//    @Test
-//    public void innerElementTest(){
-//        open("");
-//        List<SelenideElement> javascriptBooksList = $(".books-wrapper")
-//                .$$("[role=\"rowgroup\"]")
-//                .stream()
-//                .filter(el -> el.$("[role=\"gridcell\"]", 3).getText().equals("O'Reilly Media"))
-//                .filter(el -> el.$("[role=\"gridcell\"]",1).getText().toLowerCase().contains("javascript"))
-//                .toList();
-//        ElementsCollection books = $$(javascriptBooksList);
-//        books
-//                .stream()
-//                .filter(el -> el.$("img").getAttribute("src").length()>0);
-//    }
+    @Test
+    public void innerElementTest(){
+        open("");
+        ElementsCollection javascriptBooks = $(".books-wrapper").$$("[role=\"rowgroup\"]");
+        List<SelenideElement> javascriptBooksList = javascriptBooks
+                .stream()
+                .filter(el -> el.$("[role=\"gridcell\"]", 3).getText().equals("O'Reilly Media"))
+                .filter(el -> el.$("[role=\"gridcell\"]",1).getText().toLowerCase().contains("javascript"))
+                .toList();
+
+        List<SelenideElement> eachBook = javascriptBooksList
+                .stream()
+                        .filter(el -> el.lastChild().$(".rt-td", 0).lastChild().getText().equals("img")).toList();
+
+        for (SelenideElement selectedBooks :
+                eachBook) {
+            selectedBooks.$(".rt-td",0).lastChild().shouldHave(attribute("src"));
+        }
+    }
 }
+
